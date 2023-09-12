e183e41bec2a03d579f544b3527e6fdc5d71ffbe
Nakonechnyi-Maksim <nakonechnyi1234567890@gmail.com>
2023-09-13 02:55:14 +0400

    #

--- server/index.js
+++ server/index.js
@@ -1,1 +1,18 @@
-asdad;
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const cookie = require("cookie-parser");
+const { error } = require("console");
+
+const PORT = process.env.PORT;
+const app = express();
+
+const start = async () => {
+  try {
+    app.listen(PORT, () => console.log(`Сервер на этом порте: ${PORT}`));
+  } catch (e) {
+    console.log("Ашибочка");
+  }
+};
+
+start();
